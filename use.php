460f7bd393e1452bf315c8cd2dc0c5def9d5b0dd
aitor <aitor@e451.net>
2019-04-03 19:47:23 +0200

    1.1

--- use.php
+++ use.php
@@ -4,10 +4,12 @@
   Text Domain: useless
   Plugin URI:
   Description: I do not give a damn
-  Version: 1.0
+  Version: 1.1
   Author: Aitor The Great
   Author URI: https://telmo.club
   License: The Unlicense
   */
 ?>
 <?php
+
+$thing = 'a 1.1 thing';
