8c97c9a8189f6e74cc3aee5ebd76c7b1c999f1d7
aitor <aitor@e451.net>
2019-04-03 19:32:39 +0200

    1.0

---
+++ use.php
@@ -0,0 +1,13 @@
+<?php
+  /*
+  Plugin Name: useless
+  Text Domain: useless
+  Plugin URI:
+  Description: I do not give a damn
+  Version: 1.0
+  Author: Aitor The Great
+  Author URI: https://telmo.club
+  License: The Unlicense
+  */
+?>
+<?php
